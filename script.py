3e86f8d9e7eea6004b536f78fcd41c533fd333eb
ArelaVka <arelav90@mail.ru>
2019-01-26 14:29:45 +0300

    del print func

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import base64
 
-base64_encoded = 'c2hvcElkOjEyMzQuc2NpZDo0MzIxLmN1c3RvbWVyTnVtYmVyOmFiYzAwMC5zaG9wQXJ0aWNsZUlkOjU2Nzg5MC5wYXltZW50VHlwZTpBQy5vcmRlck51bWJlcjphYmMxMTExMTExLmN1c3ROYW1lOkpvaG4gRG9lLmN1c3RBZGRyOtCc0L7RgdC60LLQsCwg0LAv0Y8gMTAwLm9yZGVyRGV0YWlsczrQodGH0LDRgdGC0YzQtSDQtNC70Y8g0LLRgdC10YUsINCyINC/0LDQutC10YLQuNC60LDRhSwg0YDQvtGB0YHRi9C/0YzRjg=='
+BASE64_ENCODED = 'c2hvcElkOjEyMzQuc2NpZDo0MzIxLmN1c3RvbWVyTnVtYmVyOmFiYzAwMC5zaG9wQXJ0aWNsZUlkOjU2Nzg5MC5wYXltZW50VHlwZTpBQy5vcmRlck51bWJlcjphYmMxMTExMTExLmN1c3ROYW1lOkpvaG4gRG9lLmN1c3RBZGRyOtCc0L7RgdC60LLQsCwg0LAv0Y8gMTAwLm9yZGVyRGV0YWlsczrQodGH0LDRgdGC0YzQtSDQtNC70Y8g0LLRgdC10YUsINCyINC/0LDQutC10YLQuNC60LDRhSwg0YDQvtGB0YHRi9C/0YzRjg=='
 
 def decoding_base64(base64_data):
     final_dict ={}
@@ -14,11 +14,9 @@ def decoding_base64(base64_data):
             final_dict[key] = value
     return final_dict
 
-def print_final_dict(dictionary_to_print):
-    print(dictionary_to_print)
 
 def main():
-    print_final_dict(decoding_base64(base64_encoded))
+    print(decoding_base64(BASE64_ENCODED))
 
 
 if __name__ == '__main__':
